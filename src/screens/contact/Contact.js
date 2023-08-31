2342059897f719dbbe92ce2c32220cff5d73ac04
Casey Tran <caseymt@uci.edu>
2023-08-30 21:14:13 -0700

    the email works don't know why the formattings weird tho

--- src/screens/contact/Contact.js
+++ src/screens/contact/Contact.js
@@ -1,8 +1,114 @@
-import React from "react";
+import React, {useRef} from "react";
+import emailjs from '@emailjs/browser';
+
+import { Parallax } from "react-parallax";
+import { Input, TextField, Button, TextareaAutosize } from '@mui/material';
+
+import "./Contact.css"
 
 const Contact = () => {
+
+    const form = useRef();
+
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm('cmb_gmail', 'template_17bz3jm', form.current, 'gOHpJHGH2n6Z1wBw1')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+      };
+    
     return (
-        <div>Contact</div>
+        <>
+        <Parallax>
+        <div className="list-page">
+            <div className="content">
+                <div className="contact-page">
+                    <div className="contact-texts">
+                        <div className="contact-title">
+                            <h1>Contact Us</h1>
+                        </div>
+                        <div className="contact-desc">
+                            <p>Need to get in touch with us? Leave a message or call us today and we'll respond as soon as possible!</p>
+                        </div>
+
+                        
+                        <div className="contact-info">
+                            <div className="contact-title">
+                                <h1>Contact Information</h1>
+                            </div>
+                            <div className="contact-desc">
+                                <p>892 Horcajo Street, Milpitas, CA 95035</p>
+                                <p>Phone: (408) 416-3963  |  Fax: (888) 258-9029</p>
+                                <p>Email: cbui@comcast.net</p>
+                            </div>
+                        </div>
+                    </div>
+                    <form ref={form} onSubmit={sendEmail}>
+                        <div className="contact-boxes">
+                            <h2>Contact Form</h2>
+                            <div className="contact-inputs">
+                                <div className="contact-name">
+                                    <TextField 
+                                        type="text" 
+                                        fullWidth
+                                        id="standard-basic" 
+                                        label="First Name"
+                                        name="first_name" 
+                                        variant="standard" 
+                                        className="contact-input"/>
+                                    <TextField 
+                                        type="text"
+                                        fullWidth
+                                        id="standard-basic" 
+                                        name="last_name"
+                                        label="Last Name" 
+                                        variant="standard" />
+                                </div>
+                                <br></br>
+                                <TextField 
+                                    type="text" 
+                                    fullWidth
+                                    id="standard-basic" 
+                                    label="Subject" 
+                                    name="subject"
+                                    variant="standard" 
+                                    className="contact-input"/>
+
+                                <TextField 
+                                    type="email" 
+                                    fullWidth
+                                    id="standard-basic" 
+                                    label="Email" 
+                                    name="email"
+                                    variant="standard" 
+                                    className="contact-input"/>
+                                <br></br>
+                                <TextareaAutosize
+                                    type="text"
+                                    label="Message"
+                                    fullWidth
+
+                                    required
+                                    name="message"
+                                    variant="outlined"
+                                    placeholder='Message here...'
+                                    style={{ width: "20em" , height:"17em", borderColor:"#909090", borderWidth:1.3, 
+                                    margin: "5%"}}
+                                />
+                            </div>
+                            <Button type="submit">Submit</Button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        </Parallax>
+        </>
+        
     )
 }
 
--- src/screens/mortgage/Mortgage.js
+++ src/screens/mortgage/Mortgage.js
@@ -23,6 +23,8 @@ const Mortgage = () => {
         <Parallax>
             <div className="list-page">
                 <div className="content">
+                    <div className="mort-row">
+
                     <div className="mort-container">
                     <FadeInSection animationClassName="fade-in-section">
                         <div className="mort-title">
@@ -30,16 +32,21 @@ const Mortgage = () => {
                         </div>
                     </FadeInSection>
                     
-                    <div className="mort-desc">
-                        <FadeInSection animationClassName="fade-in-section">
-                            <h2>Types of Mortgages</h2>
-                        </FadeInSection>
-                    
-                        <FadeInSection animationClassName="fade-in-section">
-                            <p>There are many types of mortgages you can choose from. Which type you choose usually depends on the length of time you think you'll be in your home or the other financial obligations you have. If you think you'll be there for the long haul, then you may want a fixed rate mortgage with the lowest interest rate you can get.
-There may be other considerations, however. What if you have kids who are going to be entering college in 10 years? In that case, you might consider getting an adjustable rate mortgage or a mortgage with a balloon payment so you can keep your payments low for the first few years in order to save for college. Once the kids are out of college, you can refinance at the current rate. If you don't think you'll be in your home for that long, then you may also want to look at other options.</p>
-                        </FadeInSection>
+                        <div className="mort-desc">
+                            <FadeInSection animationClassName="fade-in-section">
+                                <h2>Types of Mortgages</h2>
+                            </FadeInSection>
+                        
+                            <FadeInSection animationClassName="fade-in-section">
+                                <p>There are many types of mortgages you can choose from. Which type you choose usually depends on the length of time you think you'll be in your home or the other financial obligations you have. If you think you'll be there for the long haul, then you may want a fixed rate mortgage with the lowest interest rate you can get.
+    There may be other considerations, however. What if you have kids who are going to be entering college in 10 years? In that case, you might consider getting an adjustable rate mortgage or a mortgage with a balloon payment so you can keep your payments low for the first few years in order to save for college. Once the kids are out of college, you can refinance at the current rate. If you don't think you'll be in your home for that long, then you may also want to look at other options.</p>
+                            </FadeInSection>
 
+                        </div>
+
+                    </div>
+                    <div className="mort-img">
+                        <img src="https://images.pexels.com/photos/8292793/pexels-photo-8292793.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width='75%' height='50%'></img>
                     </div>
                     </div>
                 </div>
@@ -49,29 +56,7 @@ There may be other considerations, however. What if you have kids who are going 
         <Parallax>
         <div className='list-page-color'>
             <div className="content">
-            <div className='bs-box2'>
-              
-              <div className='bs-box-text'>
-                <div className='bs-box-header'>
-                  <FadeInSection animationClassName='fade-in-section'>
-                      <h1>A complete marketing plan geared to net you the best price including:</h1>
-                    </FadeInSection>
-                </div>
-                <ul className='bs-box-desc'>
-                  <FadeInSection animationClassName='fade-in-section'>
-                    <li>Comprehensive Market Analysis</li>
-                    <li>Submission of property to three Multiple Listing Services</li>
-                    <li>Submission of Property to three Internet Sites</li>
-                    <li>Complete Information Packet on home, including interior and exterior photos, town facts, local school report and mortgage information</li>
-                    <li>Mass Mailing of information to potential buyers</li>
-                  </FadeInSection>
-                </ul>
-              </div>
 
-              <div className='bs-img-box'>
-                  <img src='https://images.pexels.com/photos/416405/pexels-photo-416405.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' width='100%' height='100%'></img>
-              </div>
-            </div>
             </div>
         </div>
         </Parallax>
