2fbe331a724ddb05559394ae04a09c2dd28030c9
Casey Tran <caseymt@uci.edu>
2023-09-12 19:17:20 -0700

    adjusted slider and footer

--- src/components/SliderItem/SliderItem.js
+++ src/components/SliderItem/SliderItem.js
@@ -10,9 +10,11 @@ function SliderItem(props) {
             <FadeInSection animationClassName="fade-in-section">
                 <img src={props.imgSrc} alt={props.name}/>
             </FadeInSection>
+            <div className="slider-body">
                 <span>{props.title}</span>
                 <span>{props.mortId}</span>
                 <span>{props.contact}</span>
+            </div>
         </>
     )
 }
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,25 +3,27 @@ import "./Footer.css"
 import MasterLogo from "../../images/Masterlogo1-modified.png"
 const Footer = () => {
     return (
-        <div className="page-foot">
-            <div className="footer-container">
-                <div className="logo">
-                    <img src={MasterLogo} alt="Logo"></img>
+        <div className="bg">
+            <div className="page-foot">
+                <div className="footer-container">
+                    <div className="logo">
+                        <img src={MasterLogo} alt="Logo"></img>
+                    </div>
+
+                    <div className="footer-text">
+                        <h1>Contact us</h1>
+                        <h2>892 Horcajo Street, Milpitas, CA 95035</h2>
+                        <h2>Phone: (408) 416-3963  |  Fax: (888) 258-9029</h2>
+                        <h2>Email: cbui@comcast.net</h2>
+                        <h2>BRE #: 01934309 | NMLS #: 1079239</h2>
+                    </div>
+
+                    <div className="copyright">
+                        <p>Copyright 2006-2023 CMB Realty & Mortgage, Inc. All Rights Reserved</p>
+                    </div>
                 </div>
 
-                <div className="footer-text">
-                    <h1>Contact us</h1>
-                    <h2>892 Horcajo Street, Milpitas, CA 95035</h2>
-                    <h2>Phone: (408) 416-3963  |  Fax: (888) 258-9029</h2>
-                    <h2>Email: cbui@comcast.net</h2>
-                    <h2>BRE #: 01934309 | NMLS #: 1079239</h2>
-                </div>
-
-                <div className="copyright">
-                    <p>Copyright 2006-2023 CMB Realty & Mortgage, Inc. All Rights Reserved</p>
-                </div>
             </div>
-
         </div>
     )
 }
